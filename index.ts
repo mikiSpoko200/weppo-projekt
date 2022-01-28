ea4f9c226b27f8f7814fde0c72ddb785bc16de99
Patryk Mazur <64105446+Papunio@users.noreply.github.com>
2022-01-28 01:40:15 +0100

    No next
    .

--- index.ts
+++ index.ts
@@ -26,53 +26,55 @@ app.use(express.static("./static"));
 *  - bazka - uwaga hasła musza być hashowane przed dodaniem (patrz wykład 10)
 * */
 
-
 // NOTE: wszystkie middle-ware, poza ostatnim, przyjmują 3-ci parametr: next: NextFunction
 
 // region routing: /
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/", (req: Request, res: Response) => {
+    console.log("homepage!!");
     res.render("index");
 });
 // endregion
 
 
 // region routing: /search/
-app.get("/search/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/search/", (req: Request, res: Response) => {
     console.log("get search");
-    res.render("search", {product_type: 'foobar'})
+    res.render("search", {product_type: 'foobar'});
 });
 
-app.post("/search/", (req: Request, res: Response, next: NextFunction) => {
-    let product_type = req.query.product_type;
-    console.log("post search");
-    res.render("search", {product_type: product_type + ' jooł'})
-});
+// app.post("/search/", (req: Request, res: Response, next: NextFunction) => {
+//     let product_type = req.query.product_type;
+//     console.log("post search");
+//     res.render("search", {product_type: product_type + ' jooł'})
+//     next();
+// });
 // endregion
 
 
 // region routing: /browse/
-app.use("/browse/", (req: Request, res: Response, next: NextFunction) => {
+app.get("browse/", (req: Request, res: Response) => {
     res.render("browse");
 });
 // endregion
 
 
 // region routing: /login/
-app.use("/login/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/login", (req: Request, res: Response) => {
+    console.log("login");
     res.render("login");
 });
 // endregion
 
 
 // region routing: /register/
-app.use("/register/", (req: Request, res: Response, next: NextFunction) => {
+app.get("/register/", (req: Request, res: Response) => {
     res.render("login");
 });
 // endregion
 
 
 // region routing: /product/
-app.use("/product/", (req: Request, res: Response, next: NextFunction) => {
+app.use("/product/", (req: Request, res: Response) => {
     res.render("product", {product_id: 1234321});
 });
 // endregion
@@ -81,6 +83,7 @@ app.use("/product/", (req: Request, res: Response, next: NextFunction) => {
 // region routing: 404
 app.use((req: Request, res: Response) => {
     res.render('404.ejs', {url: req.url});
+    //res.end("nie ma!");
 });
 // endregion
 
