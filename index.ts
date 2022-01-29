77f076454515a33d415e4c2d2cefa4993330e3af
mikiSpoko200 <328690@uwr.edu.pl>
2022-01-29 03:43:33 +0100

    Basic session management system, user and card model

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
+import session from 'express-session';
+import { User, Cart } from "./routes/login";
+import pg from "pg";
+
+const pool = new pg.Pool({
+        host: "localhost",
+        database: "lista8",
+        user: "postgres",
+        password: "password"
+});
+
 
 const app = express.default();
+
+
+const SIGNATURE = '64655bbc4bc11794cc67c300beaba73ab4ea957e6e2b3d1430bce3480fdf3dc997a1f174abe8fa4ce5986588e2013c6d5ae046937557e12e491783f1343e7d5c';
 
 // default app configuration.
 const enum AppConfig {
@@ -15,30 +29,26 @@ app.set(AppConfig.ViewDirectory, './views');
 
 app.use(express.static('./static'));
 app.use(express.urlencoded({extended:true}));
+app.use(session({resave:true, saveUninitialized: true, secret: SIGNATURE}));
+
+
+declare global {
+  namespace Express {
+    interface Session {
+      user: User
+    }
+  }
+}
 
 /*
  * TODO: 28.01.2020:
  *  =--------------- EJS i baza
  *  2. Podpinanie bazki - wyświetlanie produktów, filtrowanie - rosnąco/malejąco względem X, po gatunkach, czy dostępne, tytuł, reżyser, kraj produkcji.
  *  3. Wybieranie produktu podczas przeglądania.
- *  =--------------- Sesja
- *  4. Sesja na ciastkach
- *  5. koszyk
  *  =-------------- Bezpieczne hasła
  *  6. logowanie zarówno użytkownik jak i admin
  *  7. rejestrowanie zarówno użytkownik, admina rejestrujemy my albo coś.
- *
- * Użytkownik:
- *    musi móc:
- *  - stworzyć konto
- *  - dodawać przedmioty do koszyka
- *  -
- *
- *
- *
  */
-
-
 
 /*
 * TODO:
@@ -51,7 +61,18 @@ app.use(express.urlencoded({extended:true}));
 *  - bazka - uwaga hasła musza być hashowane przed dodaniem (patrz wykład 10)
 * */
 
-// NOTE: wszystkie middle-ware, poza ostatnim, przyjmują 3-ci parametr: next: NextFunction
+
+async function get_user(email: string): Promise<User> {
+    try {
+        let query_result = await pool.query('select (surname, email, password) from users');
+        let user = query_result.rows[0];
+        let cart = new Cart();
+        return new Promise(User(user.email, user.password, cart));
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 // region routing: /
 app.get('/', (req: Request, res: Response) => {
@@ -71,19 +92,28 @@ app.get('/browse/', (req: Request, res: Response) => {
 // region routing: /login/
 app.get('/login', (req: Request, res: Response) => {
     console.log('login');
-    res.render('login');
+    if (!req.session!.user) {
+        console.log("No session data found.");
+    } else {
+        console.log("Session data found");
+        console.log(`${!req.session?.user.email}, ${!req.session?.user.password}`);
+    }
+    res.render('login', { email: req.session?.user?.email, password: req.session?.user?.password });
 });
 
 app.post('/login', (req: Request, res: Response) => {
-    console.log(req.body, "adawe a");
-    res.render('login');
+    console.log(req.body)
+    req.session!.user!.email = req.body.email;
+    req.session!.user!.password = req.body.password;
+    console.log(`Posted: ${req.session?.user?.email}, ${req.session?.user?.password}`);
+    res.redirect('browse')
 });
 // endregion
 
 
 // region routing: /cart/
 app.get('/cart/', (req: Request, res: Response) => {
-    res.render('cart');
+    res.render('cart', req.session?.user?.cart.products);
 });
 // endregion
 
--- routes/login.ts
+++ routes/login.ts
@@ -1,3 +1,30 @@
-// W tym pliku będzie logika logowania.
+import { Request, Response } from 'express';
+import * as crypto from 'crypto';
 
-console.log("Hello from login.ts");
+
+// Model of a card
+export class Cart {
+    products: string[] = [];
+
+    add_item(product_id: string) {
+        this.products.push(product_id);
+    }
+
+    remove_item(product_id: string) {
+        this.products.filter(product => product != product_id);
+    }
+}
+
+
+// Model for user session content.
+export class User {
+    constructor(email: string, password: string, cart: Cart, ) {
+        this.email = email;
+        this.password = password;
+        this.cart = cart;
+    }
+
+    cart: Cart;
+    email: string;
+    password: string;
+}
